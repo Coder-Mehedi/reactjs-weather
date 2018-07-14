a995dd14fde9c8be4f89f56d13df076b7ec1f93f
Mehedi Hasan <mehedi5051@gmail.com>
2018-07-14 04:33:29 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ class App extends React.Component {
     const country = e.target.elements.country.value
     const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await api_call.json()
-    console.log(data)
     this.setState({
       temperature: data.main.temp,
       city: data.name,
